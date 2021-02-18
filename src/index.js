0271f0a8de42f8beb211a5441b6c40653531db5a
MaksimLuzhinskiy <maks.luzhinskiy00@gmail.com>
2021-02-18 22:05:25 +0300

    fetch: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n=Math.abs(n);
+	n = String(n).split('').reverse();
+	console.log(n);
+	return n[0]== 0 ? n.splice(1,n.length).join('') : n.join('');  
+    
 }
